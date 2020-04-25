75bd3f658f91f5eb39fd4a186b6521b1d8a6cda6
Trenton Morgan <tpm7qq@mst.edu>
2020-04-25 14:10:07 -0500

    Add homepage and project management functions
    Includes homepage display (project buttons are wonky, I want them to be a table), create a project, and edit/delete an existing project

--- root.py
+++ root.py
@@ -1,143 +1,366 @@
-from tkinter import *
+#Import sqlite3 database tools
 import sqlite3
 
-root = Tk()
-root.title("Bug Tracker")
-root.geometry("400x400")
+#Import tkinter GUI module (may be Tkinter on some systems)
+try:
+    from tkinter import *
+except(ImportError):
+    from Tkinter import *
 
-#Connect to database
-conn = sqlite3.connect("bugTracker.db")
+#Import time functions
+from datetime import datetime
 
+#Flag indicates whether or not to populate the database tables with example data on creation
+populate_tables = "False"
+
+###INITIALIZE DATABASE###
+#Connect to database info file
+conn = sqlite3.connect("info.db")
 #Create database cursor
 c = conn.cursor()
 
-#Create Ticket table if not already present
+#All tables are created only on the first run of the app
+#Create Ticket table
 try:
-    c.execute("""CREATE TABLE Tickets (
-        ticket_id integer,
-        title text,
-        description text,
-        date_created integer,
-        status text,
-        priority text,
-        ticket_type text
-        )""")
+    c.execute("""
+        CREATE TABLE Tickets (
+        ticket_ID Integer PRIMARY KEY,
+        title Integer,
+        description Text,
+        ticket_type Text,
+        priority Text,
+        status Text,
+        date_created Text,
+        FOREIGN KEY(project_ID) REFERENCES Projects
+        	ON DELETE CASCADE
+        )
+    	""")
 except(sqlite3.OperationalError):
     print("Table \'Tickets\' already exists")
 
+#Create Projects table
+try:
+    c.execute("""
+        CREATE TABLE Projects (
+        project_ID Integer PRIMARY KEY,
+        name Text,
+        description Text,
+        ticket_count Integer,
+        date_created Text
+        )
+    	""")
+except(sqlite3.OperationalError):
+    print("Table \'Projects\' already exists")
+
 #Commit changes
 conn.commit()
-
 #Close connection
 conn.close()
 
-#Submit function
-def submit():
-    #Connect to database
-    conn = sqlite3.connect("bugTracker.db")
+###EXAMPLE SET INSERTION###
+#Example set to be used for demonstration
+#TBD
 
-    #Create database cursor
-    c = conn.cursor()
-
-    #Insert into table
-    c.execute("""INSERT INTO Tickets VALUES (:ticket_id, :title, :description,
-        :date_created, :status, :priority, :ticket_type)""",
-        {
-            "ticket_id": ticket_id.get(),
-            "title": title.get(),
-            "description": description.get(),
-            "date_created": date_created.get(),
-            "status": status.get(),
-            "priority": priority.get(),
-            "ticket_type": ticket_type.get()
-        })
-
-    #Commit changes
-    conn.commit()
-
-    #Close connection
-    conn.close()
-
-    #Clear text boxes
-    ticket_id.delete(0, END)
-    title.delete(0, END)
-    description.delete(0, END)
-    date_created.delete(0, END)
-    status.delete(0, END)
-    priority.delete(0, END)
-    ticket_type.delete(0, END)
-
+#Populate function uses the given example data to be inserted into the database after initialization
+def populate():
+    print("Populate Function Called")
     return
 
-#Query function
-def query():
-    #Connect to database
-    conn = sqlite3.connect("bugTracker.db")
+###HELPER FUNCTIONS###
+#Count the total number of projects present in the database
+def Count_Projects():
+	#Connect to DB file
+	conn = sqlite3.connect("info.db")
+	#Create database cursor
+	c = conn.cursor()
 
-    #Create database cursor
-    c = conn.cursor()
+	#Get the count for projects in the Projects table
+	c.execute("SELECT Count(*) FROM Projects")
+	proj_count = c.fetchone()
+	#Cursor returns a tuples, so get the first value
+	proj_count = proj_count[0]
 
-    #Query the database
-    c.execute("SELECT oid, * FROM Tickets")
-    tickets = c.fetchall()
-    #print(tickets)
+	#Close connection
+	conn.close()
 
-    #Loop thru results and output them to screen
-    print_tickets = ""
-    for ticket in tickets:
-        print_tickets += str(ticket) + "\n"
+	#Return the count
+	return proj_count
 
-    query_label = Label(root, text=print_tickets)
-    query_label.grid(row=9, column=0, columnspan=2)
+#Generate a button for the project with ID 'proj_ID'
+def Generate_Button(proj_ID):
+	#Connect to DB file
+	conn = sqlite3.connect("info.db")
+	#Create database cursor
+	c = conn.cursor()
+
+	#Select the project tuple with matching ID
+	c.execute("SELECT * FROM Projects WHERE project_ID = ?", (proj_ID,))
+	proj_info = c.fetchone()
+	print(proj_info)
+
+	#Create frame for the project info
+	proj_frame = Frame(root, width=300, height=100, bd=5, highlightcolor="black", highlightthickness=5)
+	#Disable resizing of frame
+	proj_frame.grid_propagate(False)
+	#Enable button to fill frame
+	proj_frame.columnconfigure(0, weight=1)
+	proj_frame.rowconfigure(0, weight=1)
+	proj_frame.grid(columnspan=2)
+
+	#Create and place button
+	btn_text = str(proj_info[0]) + "\t\t" + str(proj_info[1])
+	proj_btn = Button(proj_frame, text=btn_text, command=lambda: View_Project(proj_ID))
+	proj_btn.grid(row=0, column=0)
+
+	#Place description beneath button
+	desc_label = Label(proj_frame, text="-- " + proj_info[2])
+	desc_label.grid(row=1, column=0, sticky=N+W)
+
+	#Close connection
+	conn.close()
+
+	return
+
+###BUTTON FUNCTIONS###
+#Create a new project and insert it into the database
+def Create_Project():
+	#Open new window
+	proj_window = Toplevel()
+	proj_window.geometry("300x300")
+	proj_window.title("Create New Project")
+
+	#Place title label
+	title_frame = Frame(proj_window, height=10, width=200, bd=5)
+	title_frame.grid(row=0, column=0, pady=5)
+	title_label = Label(title_frame, text="CREATE NEW PROJECT")
+	title_label.pack()
+
+	#Place entry box and label for "name" attribute
+	name_label = Label(proj_window, text="Project Name*")
+	name_label.grid(row=1, column=0, sticky=W)
+	name_box = Entry(proj_window, width=30)
+	name_box.grid(row=2, column=0)
+
+	#Place entry box and label for "description" attribute
+	description_label = Label(proj_window, text="Description")
+	description_label.grid(row=3, column=0, sticky=W)
+	description_box = Entry(proj_window, width=30)
+	description_box.grid(row=4, column=0)
+
+	#Place submit button
+	submit_btn = Button(proj_window, text="CREATE PROJECT", command=lambda: Submit_Project(name_box, description_box, proj_window))
+	submit_btn.grid(row=5, column=0)
+
+	return
+
+#Insert a project with the given field values into the Projects table
+def Submit_Project(name_box, description_box, proj_window):
+	#Connect to database info file
+	conn = sqlite3.connect("info.db")
+	#Create database cursor
+	c = conn.cursor()
+
+	#Get the current date
+	curr_date = datetime.now()
+	curr_date.isoformat()
+	#Only keep YYYY-MM-DD
+	curr_date = str(curr_date)[:10]
+
+	#Insert the new project
+	proj_data = (name_box.get(), description_box.get(), 0, curr_date)
+	c.execute("""INSERT INTO Projects(name, description, ticket_count, date_created) VALUES
+				(?, ?, ?, ?)""", proj_data)
+
+	#Clear text boxes
+	name_box.delete(0, END)
+	description_box.delete(0, END)
+
+	#Close proj_window
+	proj_window.destroy()
+
+	#Commit changes
+	conn.commit()
+	#Close connection
+	conn.close()
+
+	return
+
+#Open "Manage Projects" window to allow deletion and editing of projects
+def Manage_Projects():
+	#Open new window
+	proj_manage_window = Toplevel()
+	proj_manage_window.geometry("300x300")
+	proj_manage_window.title("Manage Projects")
+
+	#Place title label
+	title_frame = Frame(proj_manage_window, height=10, width=200, bd=5)
+	title_frame.grid(row=0, column=0, pady=5)
+	title_label = Label(title_frame, text="MANAGE PROJECTS")
+	title_label.pack()
+
+	#Determine which project is to be edited
+	manage_label = Label(proj_manage_window, text="ID of project to edit:")
+	manage_label.grid(row=1, column=0)
+	manage_box = Entry(proj_manage_window, width=10)
+	manage_box.grid(row=1, column=1)
+	manage_btn = Button(proj_manage_window, text="EDIT PROJECT", command=lambda: Edit_Project(manage_box.get(), manage_label, manage_box, manage_btn, proj_manage_window))
+	manage_btn.grid(row=2, column=0)
+
+	return
+
+#Edit the details of a project
+def Edit_Project(proj_ID, manage_label, manage_box, manage_btn, proj_manage_window):
+	#Connect to database info file
+	conn = sqlite3.connect("info.db")
+	#Create database cursor
+	c = conn.cursor()
+
+	#Ensure project exists
+	c.execute("SELECT Count(*) FROM Projects WHERE project_ID=?", (proj_ID,))
+	count = str(c.fetchone()[0])
+	if (count == "0"):
+		print("Project does not exist")
+		return
+
+	#Close connection
+	conn.close()
+
+	#Clear label, box, and button from selection
+	manage_label.destroy()
+	manage_box.destroy()
+	manage_btn.destroy()
+
+	#Place entry box and label for "name" attribute
+	name_label = Label(proj_manage_window, text="Project Name*")
+	name_label.grid(row=1, column=0, sticky=W)
+	name_box = Entry(proj_manage_window, width=30)
+	name_box.grid(row=2, column=0, columnspan=2)
+
+	#Place entry box and label for "description" attribute
+	description_label = Label(proj_manage_window, text="Description")
+	description_label.grid(row=3, column=0, sticky=W)
+	description_box = Entry(proj_manage_window, width=30)
+	description_box.grid(row=4, column=0, columnspan=2)
+
+	#Place buttons for saving changes, cancelling process, and deleting project
+	save_btn = Button(proj_manage_window, text="SAVE CHANGES", command=lambda: Save_Changes(proj_ID, name_box, description_box, proj_manage_window))
+	save_btn.grid(row=5, column=0)
+	cancel_btn = Button(proj_manage_window, text="CANCEL", command=lambda: Cancel_Edit(proj_manage_window))
+	cancel_btn.grid(row=5, column=1)
+	delete_btn = Button(proj_manage_window, text="DELETE PROJECT", command=lambda: Delete_Project(proj_ID, proj_manage_window))
+	delete_btn.grid(row=6, column=0)
+
+	return
+
+#Save changes from edited project
+def Save_Changes(proj_ID, name_box, description_box, proj_manage_window):
+	#Connect to database info file
+	conn = sqlite3.connect("info.db")
+	#Create database cursor
+	c = conn.cursor()
+
+	#Update the project record with matching proj_ID
+	c.execute("UPDATE Projects SET name=?, description=? WHERE project_ID=?", (name_box.get(), description_box.get(), proj_ID))
+	print("Changes saved")
+
+	#Commit changes
+	conn.commit()
+	#Close connection
+	conn.close()
+
+	#Close the project manager
+	proj_manage_window.destroy()
+
+	return
+
+#Cancel changes and close window
+def Cancel_Edit(proj_manage_window):
+	#Close the project manager
+	proj_manage_window.destroy()
+
+	return
+
+#Delete a project from the database
+def Delete_Project(proj_ID, proj_manage_window):
+	#Connect to database info file
+	conn = sqlite3.connect("info.db")
+	#Create database cursor
+	c = conn.cursor()
+
+	#Update the project record with matching proj_ID
+	c.execute("DELETE FROM Projects WHERE project_ID=?", (proj_ID))
+	print("Project deleted")
+
+	#Commit changes
+	conn.commit()
+	#Close connection
+	conn.close()
+
+	#Close the project manager
+	proj_manage_window.destroy()
+
+	return
 
 
-    #Commit changes
-    conn.commit()
+#View a project
+def View_Project(proj_ID):
+	print("viewing project #", proj_ID)
+	return
 
-    #Close connection
-    conn.close()
+###CREATE HOMEPAGE###
+#Create root window
+root = Tk()
+root.title("Bug Tracker")
+root.geometry("500x500")
 
-    return
+#Place title frame
+title_frame = Frame(root, height=10, width=500, bd=5)
+title_frame.grid(row=0, column=0, columnspan=2, pady=5)
+title_label = Label(title_frame, text="WELCOME TO THE BUG TRACKER!")
+title_label.grid(row=0, column=0, columnspan=2, sticky=N+S+E+W)
 
-#Create text boxes
-ticket_id = Entry(root, width=30)
-ticket_id.grid(row=0, column=1, padx=20)
-title = Entry(root, width=30)
-title.grid(row=1, column=1)
-description = Entry(root, width=30)
-description.grid(row=2, column=1)
-date_created = Entry(root, width=30)
-date_created.grid(row=3, column=1)
-status = Entry(root, width=30)
-status.grid(row=4, column=1)
-priority = Entry(root, width=30)
-priority.grid(row=5, column=1)
-ticket_type = Entry(root, width=30)
-ticket_type.grid(row=6, column=1)
+#Place "Create New Project" button
+create_proj_btn = Button(root, text="Create New Project", command=Create_Project)
+create_proj_btn.grid(row=1, column=0, padx=10)
 
-#Create text box labels
-ticket_id_label = Label(root, text="Ticket ID")
-ticket_id_label.grid(row=0, column=0)
-title_label = Label(root, text="Title")
-title_label.grid(row=1, column=0)
-description_label = Label(root, text="Description")
-description_label.grid(row=2, column=0)
-date_created_label = Label(root, text="Date Created")
-date_created_label.grid(row=3, column=0)
-status_label = Label(root, text="Status")
-status_label.grid(row=4, column=0)
-priority_label = Label(root, text="Priority")
-priority_label.grid(row=5, column=0)
-ticket_type_label = Label(root, text="Ticket Type")
-ticket_type_label.grid(row=6, column=0)
+#Place "Manage Projects" button
+manage_proj_btn = Button(root, text="Manage Projects", command=Manage_Projects)
+manage_proj_btn.grid(row=1, column=1, padx=10, ipadx=3)
 
-#Create submit button
-submit_btn = Button(root, text="Submit Ticket", command=submit)
-submit_btn.grid(row=7, column=0, columnspan=2, pady=10, padx=10)
+#Place "Your Projects" label
+your_proj_label = Label(root, text="Your Projects")
+your_proj_label.configure(font="TkDefaultFont 10 underline")
+your_proj_label.grid(row=2, column=0, columnspan=2, pady=5, sticky=N+S+E+W)
 
-#Create query button
-query_btn = Button(root, text="Show All Tickets", command=query)
-query_btn.grid(row=8, column=0, columnspan=2, pady=10, padx=10)
+#Generate and place buttons for each of the user's projects
+proj_count = Count_Projects()
+if (proj_count == 0):
+	#Display "No Projects Found" instead of buttons
+	no_proj_label = Label(root, text="No Projects Found")
+	no_proj_label.grid(row=3, column=0, columnspan=2)
+else:
+	#Connect to database info file
+	conn = sqlite3.connect("info.db")
+	#Create database cursor
+	c = conn.cursor()
 
-#Enter event loop 
+	#Get a list of all current project IDs
+	c.execute("SELECT project_ID FROM Projects")
+	ID_list = c.fetchall()
+	print(ID_list)
+
+	#Display projects (currently a button each)
+    #I would like the projects to be output as a table but have yet to do that - use a Treeview widget
+    #tree = Treeview(master, columns=('Position', 'Name', 'Score'), show='headings')
+    #tree.heading(<column>, text="Label")
+    #tree.insert("", "end", values=(<position>, <name>, <score>))
+    #https://stackoverflow.com/questions/50625306/what-is-the-best-way-to-show-data-in-a-table-in-tkinter/50651988#50651988
+	for x in ID_list:
+		Generate_Button(x[0])
+
+	#Close connection
+	conn.close()
+
+
+#Enter event loop
 root.mainloop()
